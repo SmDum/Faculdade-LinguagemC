0d0de4807dbf2849f2d348938db1a2552bea471e
SmDum <samueldemoraesdelgado2005@gmail.com>
2024-10-24 17:35:20 -0300

    Tarefa 1

--- Exercícios/main.c
+++ Exercícios/main.c
@@ -2,37 +2,129 @@
 #include <stdlib.h>
 #include <locale.h>
 
-typedef struct hospede {
+typedef struct hospede
+{
     int quarto;
     char nome[80];
     int acompanhante;
     char categoria;
     int dias;
-}hospede;
+} hospede;
 
-typedef struct quarto {
+typedef struct quarto
+{
     int num;
     char categoria;
     char status;
-}quarto;
+} quarto;
 
-void aloca_quarto(quarto **quarto, int quant_quartos);
-void aloca_hospede(hospede **hospede, int quant_hospedes);
+void aloca_quarto(quarto **p_quarto, int quant_quartos);
+void aloca_hospede(hospede **p_hospede, int quant_hospedes);
 
-void cadastra_quarto(quarto *quarto, int quant_quartos);
-void cadastra_hospede(hospede *hospede, quarto *quarto, int quant_quarto);
+void cadastra_quarto(quarto *p_quarto, int quant_quartos);
+void cadastra_hospede(hospede *p_hospede, quarto *quarto, int quant_quarto);
 
-int  busca_quarto(quarto *quarto, int quant_quarto, char categoria);
-int  busca_hospede(hospede *hospede, int quant_hosp, int num_quarto);
+int busca_quarto(quarto *p_quarto, int quant_quarto, char categoria);
+int busca_hospede(hospede *p_hospede, int quant_hosp, int num_quarto);
 
-void check_out(hospede *hospede, int quant_hosp, quarto *quarto);
+void check_out(hospede *p_hospede, int quant_hosp, quarto *quarto);
 
-void mostra_quarto(quarto *quarto, int quant_quarto);
-void mostra_hospede(hospede *hospede, int quant_hospede);
+void mostra_quarto(quarto *p_quarto, int quant_quarto);
+void mostra_hospede(hospede *p_hospede, int quant_hospede);
 
 int main()
 {
     setlocale(LC_ALL, "portuguese");
 
+    quarto *p_quarto = NULL;
+    hospede *p_hospede = NULL;
+    int opc, cont = 0, quant = 15, pos;
+
+    do
+    {
+        system("cls");
+
+        printf("//////HOTEL FAZENDA RIBEIRAO//////\n\n[1]Check-in\n[2]Check-out\n[3]Mostra Quarto\n[4]Mostra Hospede\n[5]Fim\nOpcao: ");
+        scanf("%i", &opc);
+        fflush(stdin);
+
+        switch (opc)
+        {
+        case 1:
+            pos = busca_hospede(p_hospede, cont, -1);
+            if (pos == -1)
+            {
+                aloca_hospede(&p_hospede, cont + 1);
+                cadastra_hospede(p_hospede + cont, p_quarto, quant);
+                cont++;
+            }
+            cadastra_hospede(p_hospede + pos, p_quarto, quant);
+            break;
+
+        case 2:
+            check_out(p_hospede, cont, p_quarto);
+            break;
+
+        case 3:
+            mostra_quarto(p_quarto, quant);
+            break;
+
+        case 4:
+            mostra_hospede(p_quarto, cont);
+            break;
+        }
+    
+    } while (opc != 5);
+
+    free(p_quarto);
+    free(p_hospede);
+
     return 0;
 }
+
+void aloca_quarto(quarto **p_quarto, int quant_quartos)
+{
+    if ((*p_quarto = (quarto *)realloc(*p_quarto, quant_quartos * sizeof(quarto)) == NULL))
+    {
+        printf("Erro ao Alocar Memória...");
+        exit(1);
+    }
+}
+void aloca_hospede(hospede **p_hospede, int quant_hospedes)
+{
+    if ((*p_hospede = (hospede *)realloc(*p_hospede, quant_hospedes * sizeof(hospede)) == NULL))
+    {
+        printf("Erro ao Alocar Memória...");
+        exit(1);
+    }
+}
+
+void cadastra_quarto(quarto *p_quarto, int quant_quartos)
+{
+    int i;
+
+    for(i=0;i<quant_quartos;i++, p_quarto++)
+    {
+        p_quarto -> num = i+1;
+        p_quarto -> status = 'L';
+        
+        if(i<5)
+        {
+            p_quarto -> categoria = 'S';
+        }
+        else
+        {
+            p_quarto -> categoria = 'F';
+        }
+    }
+}
+
+void cadastra_hospede(hospede *p_hospede, quarto *quarto, int quant_quarto);
+
+int busca_quarto(quarto *p_quarto, int quant_quarto, char categoria);
+int busca_hospede(hospede *p_hospede, int quant_hosp, int num_quarto);
+
+void check_out(hospede *p_hospede, int quant_hosp, quarto *quarto);
+
+void mostra_quarto(quarto *p_quarto, int quant_quarto);
+void mostra_hospede(hospede *p_hospede, int quant_hospede);
