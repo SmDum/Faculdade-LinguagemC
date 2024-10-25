90f40dcf50910ec27053729e6c731b9fc1f87328
SmDum <samueldemoraesdelgado2005@gmail.com>
2024-10-25 08:55:38 -0300

    Aula Prática

--- Faculdade/18-10-2024/hotel_melhorado.c
+++ Faculdade/18-10-2024/hotel_melhorado.c
@@ -1,215 +1,247 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-//struct
+// struct
 
-typedef struct hospede{
-	int  quarto;		// número do quarto
+typedef struct hospede
+{
+	int quarto; // número do quarto
 	char nome[30];
-	int  acompanhante;	// quantidade de acompanhantes
-	char categoria; 	// [S]olteiro / [F]amiliar
-	int  dias; 			// tempo de permanência – em dias
-}hospede;
+	int acompanhante; // quantidade de acompanhantes
+	char categoria;	  // [S]olteiro / [F]amiliar
+	int dias;		  // tempo de permanência – em dias
+} hospede;
 
-typedef struct quarto{
-	int num; 			// número do quarto
-	char categoria; 	// [S]olteiro / [F]amiliar
-	char status; 		// [L]ivre / [O]cupado
-}quarto;
+typedef struct quarto
+{
+	int num;		// número do quarto
+	char categoria; // [S]olteiro / [F]amiliar
+	char status;	// [L]ivre / [O]cupado
+} quarto;
 
-//prototipos
+// prototipos
 void aloca_quarto(quarto **q, int quant_quartos);
 void aloca_hospede(hospede **h, int quant_hospedes);
 
 void cadastra_quarto(quarto *q, int qq);
 void cadastra_hospede(hospede *h, quarto *q, int qq);
 
-int  busca_quarto(quarto *q, int qq, char categoria);
-int  busca_hospede(hospede *h, int qh, int n_quarto);
+int busca_quarto(quarto *q, int qq, char categoria);
+int busca_hospede(hospede *h, int qh, int n_quarto);
 
 void check_out(hospede *h, int qh, quarto *q);
 
 void mostra_quarto(quarto *q, int qq);
 void mostra_hospede(hospede *h, int qh);
 
-int main(){
-	
+int main()
+{
+
 	quarto *p_quarto = NULL;
 	hospede *p_hospede = NULL;
 	int op, cont = 0, quant = 15, pos;
-	
-	aloca_quarto(&p_quarto, quant);		//aloca o espaco para os quartos
-	cadastra_quarto(p_quarto, quant);	//cadastra os quartos como L e S/F
-	
-	do{
+
+	aloca_quarto(&p_quarto, quant);	  // aloca o espaco para os quartos
+	cadastra_quarto(p_quarto, quant); // cadastra os quartos como L e S/F
+
+	do
+	{
 		system("cls");
-		
+
 		printf("//////HOTEL FAZENDA RIBEIRAO//////\n\n[1]Check-in\n[2]Check-out\n[3]Mostra Quarto\n[4]Mostra Hospede\n[5]Fim\nOpcao: ");
 		scanf("%i", &op);
 		fflush(stdin);
-		
-		switch(op){
-			case 1: pos = busca_hospede(p_hospede, cont, -1);
-					if(pos == -1){
-						aloca_hospede(&p_hospede, cont+1);
-						cadastra_hospede(p_hospede + cont, p_quarto, quant);
-						cont++;
-					}
-					cadastra_hospede(p_hospede+pos, p_quarto, quant);
-					break;
-			case 2: check_out(p_hospede, cont, p_quarto);
-					break;
-			case 3: mostra_quarto(p_quarto, quant);
-					break;
-			case 4: mostra_hospede(p_hospede, cont);
-					break;
-			
-		}//switch
-		
-	}while(op != 5);
-		
+
+		switch (op)
+		{
+		case 1:
+			pos = busca_hospede(p_hospede, cont, -1);
+			if (pos == -1)
+			{
+				aloca_hospede(&p_hospede, cont + 1);
+				cadastra_hospede(p_hospede + cont, p_quarto, quant);
+				cont++;
+			}
+			cadastra_hospede(p_hospede + pos, p_quarto, quant);
+			break;
+		case 2:
+			check_out(p_hospede, cont, p_quarto);
+			break;
+		case 3:
+			mostra_quarto(p_quarto, quant);
+			break;
+		case 4:
+			mostra_hospede(p_hospede, cont);
+			break;
+
+		} // switch
+
+	} while (op != 5);
+
 	return 0;
-}//main
+} // main
 
-void aloca_quarto(quarto **q, int quant_quartos){
-	if((*q = (quarto*)realloc(*q, quant_quartos * sizeof(quarto))) == NULL)
+void aloca_quarto(quarto **q, int quant_quartos)
+{
+	if ((*q = (quarto *)realloc(*q, quant_quartos * sizeof(quarto))) == NULL)
 		exit(1);
-	
-}//aloca_quarto
 
-void aloca_hospede(hospede **h, int quant_hospedes){
-	if((*h = (hospede*)realloc(*h, quant_hospedes * sizeof(hospede))) == NULL)
+} // aloca_quarto
+
+void aloca_hospede(hospede **h, int quant_hospedes)
+{
+	if ((*h = (hospede *)realloc(*h, quant_hospedes * sizeof(hospede))) == NULL)
 		exit(1);
-}//aloca_hospede
+} // aloca_hospede
 
-void cadastra_quarto(quarto *q, int qq){
+void cadastra_quarto(quarto *q, int qq)
+{
 	int i;
-	
-	for(i = 0; i < qq; i++, q++){
-		q->num = i+1;
-		q->status = 'L'; //todos os quartos livres
-		if(i < 5)
-			q->categoria = 'S'; //5 de solteiro
-		else
-			q->categoria = 'F'; //10 de familiar
-	}//for
-	
-}//cadastra_quarto
 
-void cadastra_hospede(hospede *h, quarto *q, int qq){
-	
+	for (i = 0; i < qq; i++, q++)
+	{
+		q->num = i + 1;
+		q->status = 'L'; // todos os quartos livres
+		if (i < 5)
+			q->categoria = 'S'; // 5 de solteiro
+		else
+			q->categoria = 'F'; // 10 de familiar
+	} // for
+
+} // cadastra_quarto
+
+void cadastra_hospede(hospede *h, quarto *q, int qq)
+{
+
 	printf("\nNome: ");
 	gets(h->nome);
 	fflush(stdin);
-	
-	do{
+
+	do
+	{
 		printf("\nQuantidade de acompanhantes (Max. 3): ");
 		scanf("%i", &(h->acompanhante));
 		fflush(stdin);
-	}while(h->acompanhante < 0 || h->acompanhante > 3);
-	
-	if(h->acompanhante == 0)
+	} while (h->acompanhante < 0 || h->acompanhante > 3);
+
+	if (h->acompanhante == 0)
 		h->categoria = 'S';
 	else
 		h->categoria = 'F';
-		
+
 	printf("\nDias: ");
 	scanf("%i", &(h->dias));
 	fflush(stdin);
-	
+
 	h->quarto = busca_quarto(q, qq, h->categoria);
-	if(h->quarto == -1)
+	if (h->quarto == -1)
 		printf("\nNao ha quartos com a categoria desejada\n\n");
 	else
 		printf("\nCadastro bem-sucedido - Seu quarto eh: %i", h->quarto);
-	
+
 	printf("\n\n\n");
 	system("pause");
-	
-}//cadastra_hospede
 
-int busca_quarto(quarto *q, int qq, char categoria){
+} // cadastra_hospede
+
+int busca_quarto(quarto *q, int qq, char categoria)
+{
 	int i;
-	for(i = 0; i < qq; i++, q++){
-		if(q->categoria == categoria && q->status == 'L'){
+	for (i = 0; i < qq; i++, q++)
+	{
+		if (q->categoria == categoria && q->status == 'L')
+		{
 			q->status = 'O';
 			return q->num;
-		}//if
-	}//for
-	
-	return -1; //nao tem quartos livres
-}//busca_quarto
+		} // if
+	} // for
 
-int busca_hospede(hospede *h, int qh, int n_quarto){
+	return -1; // nao tem quartos livres
+} // busca_quarto
+
+int busca_hospede(hospede *h, int qh, int n_quarto)
+{
 	int i;
-	for(i = 0; i < qh; i++, h++){
-		if(h->quarto == n_quarto){
-			return i; //posicao do hospede na estrutura
-		}//if
-	}//for
-	
-	return -1; //nao achou o hospede nesse quarto
-}//busca_hospede
+	for (i = 0; i < qh; i++, h++)
+	{
+		if (h->quarto == n_quarto)
+		{
+			return i; // posicao do hospede na estrutura
+		} // if
+	} // for
 
-void check_out(hospede *h, int qh, quarto *q){
+	return -1; // nao achou o hospede nesse quarto
+} // busca_hospede
+
+void check_out(hospede *h, int qh, quarto *q)
+{
 	int n_quarto, achou;
 	float valor;
-	
+
 	printf("\nQual quarto?: ");
 	scanf("%i", &n_quarto);
 	fflush(stdin);
-	
+
 	achou = busca_hospede(h, qh, n_quarto);
-	if(achou == -1){
+	if (achou == -1)
+	{
 		printf("\nEsse quarto nao existe\n\n");
-	}else{
-		if((h+achou)->categoria == 'S')
-			valor = (h+achou)->dias * 85;
+	}
+	else
+	{
+		if ((h + achou)->categoria == 'S')
+			valor = (h + achou)->dias * 85;
 		else
-			valor = (h+achou)->dias * (((h+achou)->acompanhante) + 1) * 65;
-		
-		printf("\nNome: %s\nAcompanhantes: %i\nCategoria: %c\nDias: %i\nValor a ser pago: R$ %.2f\n\n", (h+achou)->nome, (h+achou)->acompanhante, (h+achou)->categoria, (h+achou)->dias, valor);
-	}//else
-	
-	(q+n_quarto - 1)->status = 'L'; //libera o quarto
-	
+			valor = (h + achou)->dias * (((h + achou)->acompanhante) + 1) * 65;
+
+		printf("\nNome: %s\nAcompanhantes: %i\nCategoria: %c\nDias: %i\nValor a ser pago: R$ %.2f\n\n", (h + achou)->nome, (h + achou)->acompanhante, (h + achou)->categoria, (h + achou)->dias, valor);
+	} // else
+
+	(q + n_quarto - 1)->status = 'L'; // libera o quarto
+
 	system("pause");
-	
-}//check_out
 
-void mostra_quarto(quarto *q, int qq){
-	// N�mero de quartos
-    int i;
-    
-    // Linha de n�meros dos quartos
-    for(i = 1; i <= qq; i++){
-        printf("|-%s%d %c-", i < 10 ? "0" : "",(q+i-1)->num, (q+i-1)->categoria);
-    }
-    printf("|\n");
+} // check_out
 
-    // Linha L e Q
-    for(i = 1; i <= qq; i++){
-        printf("|  %c   ", (q+i-1)->status);
-    }
-    printf("|\n");
+void mostra_quarto(quarto *q, int qq)
+{
+	// N�mero de quartos
+	int i;
 
-    // Linha divis�ria
-    for(i = 1; i <= qq; i++){
-        printf("|------");
-    }
-    printf("|\n");
-    
-    system("pause");
-}//mostra_quarto
+	// Linha de n�meros dos quartos
+	for (i = 1; i <= qq; i++)
+	{
+		printf("|-%s%d %c-", i < 10 ? "0" : "", (q + i - 1)->num, (q + i - 1)->categoria);
+	}
+	printf("|\n");
 
-void mostra_hospede(hospede *h, int qh){
+	// Linha L e Q
+	for (i = 1; i <= qq; i++)
+	{
+		printf("|  %c   ", (q + i - 1)->status);
+	}
+	printf("|\n");
+
+	// Linha divis�ria
+	for (i = 1; i <= qq; i++)
+	{
+		printf("|------");
+	}
+	printf("|\n");
+
+	system("pause");
+} // mostra_quarto
+
+void mostra_hospede(hospede *h, int qh)
+{
 	int i;
 	printf("\n--LISTA DE HOSPEDES--\n");
-	for(i = 0; i < qh; i++, h++){
+	for (i = 0; i < qh; i++, h++)
+	{
 		printf("\nQuarto: %i\nNome: %s\nAcompanhantes: %d\nCategoria: %c\nDias: %d", h->quarto, h->nome, h->acompanhante, h->categoria, h->dias);
 	}
-	
+
 	printf("\n\n\n");
-	
+
 	system("pause");
 }
---
+++ Faculdade/25-10-2024/main.c
@@ -0,0 +1,125 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <locale.h>
+    #include <string.h>
+
+    typedef struct data
+    {
+        int dia;
+        int mes;
+        int ano;
+    } data;
+
+    typedef struct pessoa
+    {
+        char nome[31];
+        char fone[21];
+        data nasc;
+    } pessoa;
+
+    void aloca_pessoa(pessoa **p_pes, int cont);
+    void cadastra_pessoa(pessoa *p_pes);
+    void registra_nasc(data *nasc);
+    void mostrar_lista(pessoa *p_pes, int cont);
+    void buscar_pessoa(pessoa *p_pes, int cont);
+
+    int main()
+    {
+        setlocale(LC_ALL, "portuguese");
+
+        int quant = 0, opc;
+        pessoa *p_pessoas = NULL;
+
+        do
+        {
+
+            printf("[1] - Cadastrar Pessoas\n[2] - Buscar Pessoa\n[3] - Mostrar Lista\n[4] - Sair\n");
+            scanf("%i", &opc);
+
+            switch (opc)
+            {
+            case 1:
+                aloca_pessoa(&p_pessoas, quant + 1);
+                cadastra_pessoa(p_pessoas + quant);
+                quant++;
+                break;
+            case 2:
+                buscar_pessoa(p_pessoas, quant);
+                break;
+            case 3:
+                mostrar_lista(p_pessoas, quant);
+                break;
+            case 4:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
+            }
+        } while (opc != 4);
+
+        free(p_pessoas); // Liberar a memória alocada
+
+        return 0;
+    }
+
+    // Função para alocar memória para a lista de pessoas
+    void aloca_pessoa(pessoa **p_pes, int cont)
+    {
+        *p_pes = (pessoa *)realloc(*p_pes, cont * sizeof(pessoa));
+        if (*p_pes == NULL)
+        {
+            printf("\nErro ao alocar memória...\n");
+            exit(1);
+        }
+    }
+
+    // Função para cadastrar pessoas usando apenas ponteiros
+    void cadastra_pessoa(pessoa *p_pes)
+    {
+        pessoa *ptr = p_pes; // Ponteiro auxiliar para percorrer a lista
+        
+            printf("\nDigite o nome da pessoa: ");
+            scanf(" %s", ptr->nome); // Lê o nome com espaços
+
+            printf("Digite o telefone da pessoa: ");
+            scanf(" %s", ptr->fone); // Lê o telefone com espaços
+
+            // Registra a data de nascimento
+            registra_nasc(&(ptr->nasc));
+    }
+
+    // Função para registrar a data de nascimento usando ponteiros
+    void registra_nasc(data *nasc)
+    {
+        printf("Digite a data de nascimento (dd mm aaaa): ");
+        scanf("%d %d %d", &nasc->dia, &nasc->mes, &nasc->ano);
+    }
+
+ void mostrar_lista(pessoa *p_pes, int cont) {
+    pessoa *ptr = p_pes;
+    for (int i = 0; i < cont; i++, ptr++) {
+        printf("Nome: %s\nData de Nascimento: %02d/%02d/%d\nTelefone: %s\n",
+               ptr->nome, ptr->nasc.dia, ptr->nasc.mes, ptr->nasc.ano, ptr->fone);
+    }
+}
+
+    void buscar_pessoa(pessoa *p_pes, int cont)
+    {
+        int i;
+        char nome[31];
+
+        printf("\nDigite o nome que deseja Buscar: ");
+        fflush(stdin);
+        gets(nome);
+        fflush(stdin);
+
+        for (i = 0; i < cont; i++, p_pes++)
+        {
+            if(strcmp(nome, p_pes->nome)==0)
+            {
+                printf("\nO Telefone de %s é %s\n", p_pes->nome, p_pes->fone);
+                return;
+            }
+        }
+        printf("\nPessoa não encontrada...\n");
+    }
