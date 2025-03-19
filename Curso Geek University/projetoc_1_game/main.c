a9a1bbdb8bf182ef5872525b0c3bcd11f279a797
Samuel <samueldemoraesdelgado2005@gmail.com>
2025-03-19 10:36:05 -0300

    Exercícios

---
+++ Curso Geek University/projetoc_1_game/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+typedef struct{
+    int dificuldade;
+    int valor1;
+    int valor2;
+    int operacao;
+    int resultado;
+}Calcular;
+
+void jogar();
+void mostrarInfo(Calcular calc);
+int somar(int resposta, Calcular calc);
+int diminuir(int resposta, Calcular calc);
+int multiplicar(int resposta, Calcular calc);
+
+int pontos = 0;
+
+int main()
+{
+    //Deve ser chamado apenas 1 vez.
+    srand(time(NULL));
+    jogar();
+    return 0;
+}
+
+void jogar()
+{
+
+}
+
+void mostrarInfo(Calcular calc)
+{
+
+}
+
+int somar(int resposta, Calcular calc)
+{
+
+}
+
+int diminuir(int resposta, Calcular calc)
+{
+
+}
+
+int multiplicar(int resposta, Calcular calc)
+{
+    
+}
